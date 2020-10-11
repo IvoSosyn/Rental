428b702c2047ed374792b288b6338bf584a4379f
IvoSosyn <sosyn@seznam.cz>
2020-10-11 22:53:19 +0200

    49. Dodělat třídu EviEntita - hodnota nadpisu sloupce, hodnota ve sloupci (hlavně nalezení Attribute a jeho hodnoty), výběr sloupců k zobrazení 48. Pro tabulku Entita nachystánn EntitaController - vyladit 47. Main.java a Main.xhtml přejmenované na Evidence.java a evidence.xhtml 46. Test na hlavní pořizovací stránku - rozpracovat logiku 45. Dopsat dokumentaci tříd a metod 44. Místo pro ukládání dat účtu je definováno na \Rental\Account\<UUID uctu>     - dodělat nacitani sestav a číselníků do šablon Typentity 	- jak označit položky pro zobrazování v Evidenci v tabulce pro Typentity 	- přidat do tabulky Attribute pole, označující viditelnost v tabulce pro výběr Typentity     - doplneno nacitani parametru z web.xml pro umisteni konfiguracnich souboru uctu     Postup pro uživatelské šablony:     - vytvořit adresář s daty, xhtml soubory účtu v definovaném adresáři na disku MIMO aplikaci Rental     - po přihlášení k účtu načíst xhtml data do adresáře /WEB-INF/account/<UUID account>     - pak je teprve možné spustit zpracování
    42. Vyladit registraci
        - zvolit šablonu a ukládání dat, jsou tu nějaké chyby
    	- vyladit tlačítka Next a Back na validitu vvstupního formuláře
    40. Neresetují se detaily Typentity a Attribute po změně a nastavení null hodnot
    	- promyslet kontrolu jedinečnosti názvu Typentity.typentity
    39. Do ModelTree doplnena obsluha katalogu šablon
        - je potřeba dodělat pořízení nové šablony
    38. Opravena přístupová práva na editaci modelu v modelu User
    	- Všechny kopie a D&D záznamy Typentity a Attribute označeny "system=false" po akci přidání do DB
    37. Opraveny testy na práva editace, přidávání a mazání Typentity, Attribute
        - založen objekt User (ten je nutné @injekt do dalších Beans, aby se dal používat)
        - Nakonfigurovat dialog pro přidávání sestav k Typentity
    36. Opraveno ukládání ModelDetail.Attribute >> přes ModelTable
        -  ModelTree vylepšit validaci na jedinečnost Zkratky a platnosti datumů OD-DO - přidat "placeholder"
        -  ModelDetail vylepšit validaci na platnosti datumů OD-DO - přidat "placeholder"
    
    32. Chyba v překreslování detailu byla ve špatné definici update, místo @Form musí být :modelFormDetail
        - vyřešit obnovení hodnoty v detailu, pokud je chyba validace a uživatel si v tabulce zvolí jiný řádek s Attribute
    30. Chybuje ukládání Attribute do DB ModelDetail, ošetřit validaci datumů PlatiOd-PlatiDo
        - mazání položky Attribute v ModelTable + přidávání Attribute
    
    27. Úkoly zůstávají , započata práce na ModelTable (Attribute) plnění doplněno o načtení 1 nové Attribute
    	- vyřešit zápis po editaci detailu a mazání položky Attribute z tabulky (tlačítko "Delete") dát k detailu
    	- dopracovat v ModelTree vložení položky Typentity s polem "system" jako false, protože to voložil uživatel - zvážit dopad
    	- zvážit přechod k uživatelsky definovaným transakcím
    	- vyladit error hlášení
    	- vyladit logování přechodem na Log4j
    
    15. Napojit všechno na DB a napsat editaci
    
    10. Doma nastavena konfigurace WildFly, která funguje.
    8. Po rekonfiguraci WildFly začalo fungovat Persistence Api s Eclipse
    
    3. Hlavní menu - vyladit
    2. Pokus o Hlavní menu

--- src/main/java/cz/rental/aplikace/evidence/EviEntita.java
+++ src/main/java/cz/rental/aplikace/evidence/EviEntita.java
@@ -9,7 +9,11 @@ import cz.rental.entity.Attribute;
 import cz.rental.entity.Entita;
 import cz.rental.entity.Typentity;
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -33,7 +37,7 @@ public class EviEntita implements Serializable {
     private ArrayList<Typentity> typenties = new ArrayList<>();
     private ArrayList<Entita> entities = new ArrayList<>();
     private ArrayList<Attribute> attributes = new ArrayList<>();
-
+    private ArrayList<String> columns;
     @EJB
     cz.rental.entity.AttributeController controller;
     @EJB
@@ -43,6 +47,9 @@ public class EviEntita implements Serializable {
 
     @PostConstruct
     public void init() {
+        this.columns.add("Entita.Popis");
+        this.columns.add("Entita.Platiod");
+        this.columns.add("Entita.Platido");
     }
 
     /**
@@ -74,4 +81,79 @@ public class EviEntita implements Serializable {
     public void setAttributes(ArrayList<Attribute> attributes) {
         this.attributes = attributes;
     }
+
+    /**
+     * Metoda pro hodnoty do tabulky prehledu Entit pro nasledny vyber detailu
+     *
+     * @param entita instance Entity, pro kterou hledam hodnotu bud z jejiho
+     * pole nebo z Attributa tabulky a jejich relacnich hodnot nebo z pole
+     * Typentity
+     * @param column ve tvaru "Tabulka.pole" napr. Entita.poznamka nebo
+     * Attribute.prijemni
+     * @return hodnotu bud z relacnich tabulek pro Attribute nebo z
+     * Entita.get[metoda] nebo z pole Typentity.get[metoda]
+     */
+    public String getFieldValue(Entita entita, String column) {
+        String table = "Attribute";
+        String field = "popis";
+        String value = "";
+        Typentity tpe = null;
+        String[] tableField;
+        if (entita == null || entita.getIdtypentity() == null) {
+            return "";
+        }
+        tpe = entita.getIdtypentity();
+        if (column == null || column.isEmpty()) {
+            column = "Attribute.popis";
+        }
+        tableField = column.split(".");
+        switch (tableField.length) {
+            case 0: {
+                break;
+            }
+            case 1: {
+                field = tableField[0];
+                break;
+            }
+            case 2: {
+                table = tableField[0];
+                field = tableField[1];
+                break;
+            }
+            default:
+        }
+        // Pokud se jedna o hodnotu Attribute je potreba dohledaat v DB
+        if (table.equalsIgnoreCase("Attribute") && this.attributes != null) {
+            for (Attribute attr : this.attributes) {
+                if (attr.getAttrname().trim().equalsIgnoreCase(field.trim())) {
+                    //TO-DO: najit hodnotu v DB pro vybranou Attribute a Typentity
+                    value = attr.getPopis();
+                    ///
+                    break;
+                }
+            }
+        }
+        // Pokud se jedna o tabulku "Typentity" metoda vrati hodnotu pole z  instance Entita
+        if (table.equalsIgnoreCase("Typentity")) {
+            Method method;
+            try {
+                method = tpe.getClass().getMethod("get" + field, (Class) null);
+                value = method.invoke(tpe, (Object) null).toString();
+            } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                Logger.getLogger(EviEntita.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        // Pokud se jedna o tabulku "Entita" metoda vrati hodnotu pole z  instance Entita
+        if (table.equalsIgnoreCase("Entita")) {
+            Method method;
+            try {
+                method = entita.getClass().getMethod("get" + field, (Class) null);
+                value = method.invoke(entita, (Object) null).toString();
+            } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                Logger.getLogger(EviEntita.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return value;
+    }
+
 }
