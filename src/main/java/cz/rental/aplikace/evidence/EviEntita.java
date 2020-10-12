eb5cdee15f8a8f0ae3867fafa9b89100810551f3
sosyn <sosyn@sosyn-w10.mukrnov.cz>
2020-10-12 16:07:05 +0200

    50. Je zacyklená metoda k naplnění sloupců 49. Dodělat třídu EviEntita - hodnota nadpisu sloupce, hodnota ve sloupci 	(hlavně nalezení Attribute a jeho hodnoty), výběr sloupců k zobrazení 48. Pro tabulku Entita nachystánn EntitaController - vyladit 47. Main.java a Main.xhtml přejmenované na Evidence.java a evidence.xhtml 46. Test na hlavní pořizovací stránku - rozpracovat logiku 45. Dopsat dokumentaci tříd a metod 44. Místo pro ukládání dat účtu je definováno na \Rental\Account\<UUID uctu>     - dodělat nacitani sestav a číselníků do šablon Typentity 	- jak označit položky pro zobrazování v Evidenci v tabulce pro Typentity 	- přidat do tabulky Attribute pole, označující viditelnost v tabulce pro výběr Typentity     - doplneno nacitani parametru z web.xml pro umisteni konfiguracnich souboru uctu     Postup pro uživatelské šablony:     - vytvořit adresář s daty, xhtml soubory účtu v definovaném adresáři na disku MIMO aplikaci Rental     - po přihlášení k účtu načíst xhtml data do adresáře /WEB-INF/account/<UUID account>     - pak je teprve možné spustit zpracování
    42. Vyladit registraci
        - zvolit šablonu a ukládání dat, jsou tu nějaké chyby
    	- vyladit tlačítka Next a Back na validitu vvstupního formuláře
    40. Neresetují se detaily Typentity a Attribute po změně a nastavení null hodnot
    	- promyslet kontrolu jedinečnosti názvu Typentity.typentity
    39. Do ModelTree doplnena obsluha katalogu šablon
        - je potřeba dodělat pořízení nové šablony
    38. Opravena přístupová práva na editaci modelu v modelu User
    	- Všechny kopie a D&D záznamy Typentity a Attribute označeny "system=false" po akci přidání do DB
    37. Opraveny testy na práva editace, přidávání a mazání Typentity, Attribute
        - založen objekt User (ten je nutné @injekt do dalších Beans, aby se dal používat)
        - Nakonfigurovat dialog pro přidávání sestav k Typentity
    36. Opraveno ukládání ModelDetail.Attribute >> přes ModelTable
        -  ModelTree vylepšit validaci na jedinečnost Zkratky a platnosti datumů OD-DO - přidat "placeholder"
        -  ModelDetail vylepšit validaci na platnosti datumů OD-DO - přidat "placeholder"
    
    32. Chyba v překreslování detailu byla ve špatné definici update, místo @Form musí být :modelFormDetail
        - vyřešit obnovení hodnoty v detailu, pokud je chyba validace a uživatel si v tabulce zvolí jiný řádek s Attribute
    30. Chybuje ukládání Attribute do DB ModelDetail, ošetřit validaci datumů PlatiOd-PlatiDo
        - mazání položky Attribute v ModelTable + přidávání Attribute
    
    27. Úkoly zůstávají , započata práce na ModelTable (Attribute) plnění doplněno o načtení 1 nové Attribute
    	- vyřešit zápis po editaci detailu a mazání položky Attribute z tabulky (tlačítko "Delete") dát k detailu
    	- dopracovat v ModelTree vložení položky Typentity s polem "system" jako false, protože to voložil uživatel - zvážit dopad
    	- zvážit přechod k uživatelsky definovaným transakcím
    	- vyladit error hlášení
    	- vyladit logování přechodem na Log4j
    
    15. Napojit všechno na DB a napsat editaci
    
    10. Doma nastavena konfigurace WildFly, která funguje.
    8. Po rekonfiguraci WildFly začalo fungovat Persistence Api s Eclipse
    
    3. Hlavní menu - vyladit
    2. Pokus o Hlavní menu

--- src/main/java/cz/rental/aplikace/evidence/EviEntita.java
+++ src/main/java/cz/rental/aplikace/evidence/EviEntita.java
@@ -12,6 +12,8 @@ import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
@@ -32,16 +34,18 @@ public class EviEntita implements Serializable {
 
     static final int COUNT_ENTITA_NEW = 5;
 
+    private Entita parent = null;
     private Typentity typentity = null;
-    private Entita entita = null;
-    private ArrayList<Typentity> typenties = new ArrayList<>();
     private ArrayList<Entita> entities = new ArrayList<>();
+    private Entita selectedEntita = null;
     private ArrayList<Attribute> attributes = new ArrayList<>();
-    private ArrayList<String> columns;
+    private ArrayList<String> columns = new ArrayList<>();
     @EJB
-    cz.rental.entity.AttributeController controller;
+    cz.rental.entity.EntitaController entitaController;
     @EJB
-    cz.rental.entity.TypentityController typEntityController;
+    cz.rental.entity.AttributeController attrController;
+    @Inject
+    cz.rental.aplikace.registrace.Account account;
     @Inject
     cz.rental.aplikace.User user;
 
@@ -50,50 +54,57 @@ public class EviEntita implements Serializable {
         this.columns.add("Entita.Popis");
         this.columns.add("Entita.Platiod");
         this.columns.add("Entita.Platido");
+        //
+        this.typentity = new Typentity();
+        this.typentity.setId(UUID.fromString("cac1b920-6b4f-4d2c-8308-86fc3fef5ec3"));
+        //
+        account.setCustomerID(UUID.fromString("34416c9f-26f2-44d8-b01d-6be4d6868dba"));
+        account.setCustomerModel(this.typentity);
+        //
+        this.parent = new Entita();
+        this.parent.setId(account.getCustomerID());
+        this.parent.setIdtypentity(account.getCustomerModel());
+        loadEntities(this.parent, account.getCustomerModel());
     }
 
     /**
-     * Metoda nacte pro zadany "typentity" pole "attribute" a ulozi do pole
-     * attributes
+     * Metoda 1. nacte vsechny instance "Entita" pro Entita.idparent==parent.id,
+     * pole Attribute pro parametr "typentity" 2. prida nove instance "Entita"
+     * do pole "entities"
      *
+     *
+     * @param parent
      * @param typentity
      */
-    public void loadEntitaForTypentity(Entita entita) {
-        if (entita == null) {
+    public void loadEntities(Entita parent, Typentity typentity) {
+        if (typentity == null || parent == null) {
             return;
         }
-        this.entita = entita;
-        this.setAttributes(controller.getAttributeForTypentity(this.typentity));
-    }
-
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
-    /**
-     * @return the attributes
-     */
-    public ArrayList<Attribute> getAttributes() {
-        return attributes;
-    }
-
-    /**
-     * @param attributes the attributes to set
-     */
-    public void setAttributes(ArrayList<Attribute> attributes) {
-        this.attributes = attributes;
+        this.parent = parent;
+        this.setTypentity(typentity);
+        this.setEntities(entitaController.getEntities(this.parent));
+        this.setAttributes(attrController.getAttributeForTypentity(typentity));
+        // Nova Entita
+        Entita newEntita = new Entita();
+        newEntita.setId(UUID.randomUUID());
+        newEntita.setIdparent(parent.getId());
+        newEntita.setIdtypentity(this.getTypentity());
+        newEntita.setPopis(this.getTypentity().getPopis());
+        newEntita.setNewEntity(true);
     }
 
     /**
-     * Metoda pro hodnoty do tabulky prehledu Entit pro nasledny vyber detailu
+     * Metoda pro nadpis sloupcu tabulky prehledu Entit pro nasledny vyber
+     * detailu
      *
-     * @param entita instance Entity, pro kterou hledam hodnotu bud z jejiho
-     * pole nebo z Attributa tabulky a jejich relacnich hodnot nebo z pole
-     * Typentity
-     * @param column ve tvaru "Tabulka.pole" napr. Entita.poznamka nebo
-     * Attribute.prijemni
-     * @return hodnotu bud z relacnich tabulek pro Attribute nebo z
-     * Entita.get[metoda] nebo z pole Typentity.get[metoda]
+     * @param entita instance Entity, pro kterou hledam nadpis z Attributa.popis
+     * tabulky z nazvu pole Typentity nebo nazvu pole Entita
+     * @param column ve tvaru "Tabulka.pole" napr. "Entita.poznamka" nebo
+     * "Attribute.prijemni"
+     * @return hodnotu bud z relacnich tabulek pro Attribute.popis nebo z
+     * Entita.metoda nebo z pole Typentity.metoda
      */
-    public String getFieldValue(Entita entita, String column) {
+    public String getColumnHeader(Entita entita, String column) {
         String table = "Attribute";
         String field = "popis";
         String value = "";
@@ -123,11 +134,72 @@ public class EviEntita implements Serializable {
             default:
         }
         // Pokud se jedna o hodnotu Attribute je potreba dohledaat v DB
-        if (table.equalsIgnoreCase("Attribute") && this.attributes != null) {
-            for (Attribute attr : this.attributes) {
+        if (table.equalsIgnoreCase("Attribute") && this.getAttributes() != null) {
+            for (Attribute attr : this.getAttributes()) {
+                if (attr.getAttrname().trim().equalsIgnoreCase(field.trim())) {
+                    value = attr.getPopis();
+                    break;
+                }
+            }
+        }
+        // Pokud se jedna o tabulku "Typentity" metoda vrati hodnotu pole z  instance Entita
+        if (table.equalsIgnoreCase("Typentity")) {
+            value = field;
+        }
+        // Pokud se jedna o tabulku "Entita" metoda vrati hodnotu pole z  instance Entita
+        if (table.equalsIgnoreCase("Entita")) {
+            value = field;
+        }
+        return value;
+    }
+
+    /**
+     * Metoda pro hodnoty do tabulky prehledu Entit pro nasledny vyber detailu
+     *
+     * @param entita instance Entity, pro kterou hledam hodnotu bud z jejiho
+     * pole nebo z Attributa tabulky a jejich relacnich hodnot nebo z pole
+     * Typentity
+     * @param column ve tvaru "Tabulka.pole" napr. Entita.poznamka nebo
+     * Attribute.prijemni
+     * @return hodnotu bud z relacnich tabulek pro Attribute nebo z
+     * Entita.get[metoda] nebo z pole Typentity.get[metoda]
+     */
+    public String getColumnValue(Entita entita, String column) {
+        String table = "Attribute";
+        String field = "popis";
+        String value = "";
+        Typentity tpe = null;
+        String[] tableField;
+        if (entita == null || entita.getIdtypentity() == null) {
+            return "";
+        }
+        tpe = entita.getIdtypentity();
+        if (column == null || column.isEmpty()) {
+            column = "Attribute.popis";
+        }
+        tableField = column.split(".");
+        switch (tableField.length) {
+            case 0: {
+                break;
+            }
+            case 1: {
+                field = tableField[0];
+                break;
+            }
+            case 2: {
+                table = tableField[0];
+                field = tableField[1];
+                break;
+            }
+            default:
+        }
+        // Pokud se jedna o hodnotu Attribute je potreba dohledaat v DB
+        if (table.equalsIgnoreCase("Attribute") && this.getAttributes() != null) {
+            for (Attribute attr : this.getAttributes()) {
                 if (attr.getAttrname().trim().equalsIgnoreCase(field.trim())) {
                     //TO-DO: najit hodnotu v DB pro vybranou Attribute a Typentity
                     value = attr.getPopis();
+                    value = attrController.getAttrValue(entita, attr, (Date) null, (Date) null).toString();
                     ///
                     break;
                 }
@@ -154,6 +226,76 @@ public class EviEntita implements Serializable {
             }
         }
         return value;
+    }
+
+    /**
+     * @return the typentity
+     */
+    public Typentity getTypentity() {
+        return typentity;
+    }
+
+    /**
+     * @param typentity the typentity to set
+     */
+    public void setTypentity(Typentity typentity) {
+        this.typentity = typentity;
+    }
+
+    /**
+     * @return the entities
+     */
+    public ArrayList<Entita> getEntities() {
+        return entities;
+    }
+
+    /**
+     * @param entities the entities to set
+     */
+    public void setEntities(ArrayList<Entita> entities) {
+        this.entities = entities;
+    }
+
+    /**
+     * @return the selectedEntita
+     */
+    public Entita getSelectedEntita() {
+        return selectedEntita;
+    }
+
+    /**
+     * @param selectedEntita the selectedEntita to set
+     */
+    public void setSelectedEntita(Entita selectedEntita) {
+        this.selectedEntita = selectedEntita;
+    }
+
+    /**
+     * @return the attributes
+     */
+    public ArrayList<Attribute> getAttributes() {
+        return attributes;
+    }
+
+    /**
+     * @param attributes the attributes to set
+     */
+    public void setAttributes(ArrayList<Attribute> attributes) {
+        this.attributes = attributes;
+    }
+
+    /**
+     * @return the columns
+     */
+    public ArrayList<String> getColumns() {
+        return columns;
+    }
+
+    /**
+     * @param columns the columns to set
+     */
+    public void setColumns(ArrayList<String> columns) {
+        this.columns = columns;
     }
 
 }
